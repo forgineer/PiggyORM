dae03aa65446d03781c323525b6330b5fb71ef6d
Blake Phillips <23227382+forgineer@users.noreply.github.com>
2025-06-08 14:35:28 -0500

    Clearing out some unnecessary files  and enhancing setup.py

--- setup.py
+++ setup.py
@@ -1,64 +1,69 @@
-from __future__ import print_function
-
+from __future__ import print_function # TODO: This can probably be removed in the future
 from setuptools import setup
-import sys
 
+import sys
 import unittest
+
 
 def test_suite():
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('pony.orm.tests', pattern='test_*.py')
     return test_suite
 
-name = "pony"
-version = __import__('pony').__version__
-description = "Pony Object-Relational Mapper"
-long_description = """
-About
-=========
-Pony ORM is easy to use and powerful object-relational mapper for Python.
-Using Pony, developers can create and maintain database-oriented software applications
-faster and with less effort. One of the most interesting features of Pony is
-its ability to write queries to the database using generator expressions.
-Pony then analyzes the abstract syntax tree of a generator and translates it
-to its SQL equivalent.
 
-Following is an example of a query in Pony::
+name: str = "pony"
 
-    select(p for p in Product if p.name.startswith('A') and p.cost <= 1000)
+version: str = __import__('pony').__version__
 
-Such approach simplify the code and allows a programmer to concentrate
-on the business logic of the application.
+description: str = "Pony Object-Relational Mapper"
 
-Pony translates queries to SQL using a specific database dialect.
-Currently Pony works with SQLite, MySQL, PostgreSQL and Oracle databases.
+long_description: str = """
+    About
+    =====
+    Pony ORM is easy to use and powerful object-relational mapper for Python.
+    Using Pony, developers can create and maintain database-oriented software applications
+    faster and with less effort. One of the most interesting features of Pony is
+    its ability to write queries to the database using generator expressions.
+    Pony then analyzes the abstract syntax tree of a generator and translates it
+    to its SQL equivalent.
 
-The package `pony.orm.examples <https://github.com/ponyorm/pony/tree/orm/pony/orm/examples>`_
-contains several examples.
+    Following is an example of a query in Pony::
+        select(p for p in Product if p.name.startswith('A') and p.cost <= 1000)
 
-Installation
-=================
-::
+    Such approach simplify the code and allows a programmer to concentrate
+    on the business logic of the application.
+
+    Pony translates queries to SQL using a specific database dialect.
+    Currently Pony works with SQLite, MySQL, PostgreSQL and Oracle databases.
+
+    The package `pony.orm.examples <https://github.com/ponyorm/pony/tree/orm/pony/orm/examples>`_
+    contains several examples.
+
+    
+    Installation
+    ============
+    ::
 
     pip install pony
 
-Entity-Relationship Diagram Editor
-=============================================
-`Pony online ER Diagram Editor <https://editor.ponyorm.com>`_ is a great tool for prototyping.
-You can draw your ER diagram online, generate  Pony entity declarations or SQL script for
-creating database schema based on the diagram and start working with the database in seconds.
+    
+    Entity-Relationship Diagram Editor
+    ==================================
+    `Pony online ER Diagram Editor <https://editor.ponyorm.com>`_ is a great tool for prototyping.
+    You can draw your ER diagram online, generate  Pony entity declarations or SQL script for
+    creating database schema based on the diagram and start working with the database in seconds.
 
-Pony ORM Links:
-=================
-- Main site: https://ponyorm.com
-- Documentation: https://docs.ponyorm.com
-- GitHub: https://github.com/ponyorm/pony
-- Mailing list:  http://ponyorm-list.ponyorm.com
-- ER Diagram Editor: https://editor.ponyorm.com
-- Blog: https://blog.ponyorm.com
+    
+    Pony ORM Links:
+    =================
+    - Main site: https://ponyorm.com
+    - Documentation: https://docs.ponyorm.com
+    - GitHub: https://github.com/ponyorm/pony
+    - Mailing list:  http://ponyorm-list.ponyorm.com
+    - ER Diagram Editor: https://editor.ponyorm.com
+    - Blog: https://blog.ponyorm.com
 """
-
-classifiers = [
+classifiers: list[str] = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: Apache Software License',
@@ -75,41 +80,47 @@ classifiers = [
     'Topic :: Database'
 ]
 
-author = ', '.join([
-    'Alexander Kozlovsky <alexander.kozlovsky@gmail.com>',
-    'Alexey Malashkevich <alexey@ponyorm.org>',
-    'Alexander Tischenko <sashaaero@yandex.ru>'
+author: str = ', '.join([
+    'Alexander Kozlovsky <alexander.kozlovsky@gmail.com>'
+    , 'Alexey Malashkevich <alexey@ponyorm.org>'
+    , 'Alexander Tischenko <sashaaero@yandex.ru>'
 ])
-author_email = "team@ponyorm.com"
-url = "https://ponyorm.com"
-project_urls = {
+
+author_email: str = "team@ponyorm.com"
+
+url: str = "https://ponyorm.com"
+
+project_urls: dict[str, str] = {
     "Documentation": "https://docs.ponyorm.org",
     "Source": "https://github.com/ponyorm/pony",
 }
-licence = "Apache License Version 2.0"
 
-packages = [
+licence: str = "Apache License Version 2.0"
+
+packages: list[str] = [
     "pony",
     "pony.flask",
-    "pony.flask.example",
+    #"pony.flask.example",
     "pony.orm",
     "pony.orm.dbproviders",
-    "pony.orm.examples",
+    #"pony.orm.examples",
     "pony.orm.integration",
     "pony.orm.tests",
     "pony.thirdparty",
     "pony.utils"
 ]
 
-package_data = {
+package_data: dict[str, list[str]] = {
     'pony.flask.example': ['templates/*.html'],
     'pony.orm.tests': ['queries.txt']
 }
 
-download_url = "http://pypi.python.org/pypi/pony/"
+download_url: str = "http://pypi.python.org/pypi/pony/"
+
 
 if __name__ == "__main__":
-    pv = sys.version_info[:2]
+    pv: tuple[int, int] = sys.version_info[:2]
+    
     if pv < (3, 8) or pv > (3, 12):
         s = "Sorry, but %s %s requires Python of one of the following versions: 3.8-3.12." \
             " You have version %s"
