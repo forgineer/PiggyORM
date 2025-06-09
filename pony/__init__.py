831e07ccb9f33c24ea97a2ef201b1a414b81512e
Blake Phillips <23227382+forgineer@users.noreply.github.com>
2025-06-08 23:46:12 -0500

    Small refactoring changes to init and converting modules

--- pony/__init__.py
+++ pony/__init__.py
@@ -6,16 +6,22 @@ from os.path import dirname
 __version__ = '0.7.19'
 
 def detect_mode():
-    try: import google.appengine
-    except ImportError: pass
+    try: 
+        import google.appengine
+    except ImportError: 
+        pass
     else:
         if os.getenv('SERVER_SOFTWARE', '').startswith('Development'):
             return 'GAE-LOCAL'
+        
         return 'GAE-SERVER'
 
-    try: from mod_wsgi import version
-    except: pass
-    else: return 'MOD_WSGI'
+    try: 
+        from mod_wsgi import version
+    except: 
+        pass
+    else: 
+        return 'MOD_WSGI'
 
     main = sys.modules['__main__']
 
@@ -28,16 +34,26 @@ def detect_mode():
     if getattr(main, 'INTERACTIVE_MODE_AVAILABLE', False): # pycharm console
         return 'INTERACTIVE'
 
-    if 'flup.server.fcgi' in sys.modules: return 'FCGI-FLUP'
-    if 'uwsgi' in sys.modules: return 'UWSGI'
-    if 'flask' in sys.modules: return 'FLASK'
-    if 'cherrypy' in sys.modules: return 'CHERRYPY'
-    if 'bottle' in sys.modules: return 'BOTTLE'
+    if 'flup.server.fcgi' in sys.modules: 
+        return 'FCGI-FLUP'
+    
+    if 'uwsgi' in sys.modules: 
+        return 'UWSGI'
+    
+    if 'flask' in sys.modules: 
+        return 'FLASK'
+    
+    if 'cherrypy' in sys.modules: 
+        return 'CHERRYPY'
+    
+    if 'bottle' in sys.modules: 
+        return 'BOTTLE'
+    
     return 'UNKNOWN'
 
 MODE = detect_mode()
-
 MAIN_FILE = None
+
 if MODE == 'MOD_WSGI':
     for module_name, module in sys.modules.items():
         if module_name.startswith('_mod_wsgi_'):
@@ -46,7 +62,9 @@ if MODE == 'MOD_WSGI':
 elif MODE != 'INTERACTIVE':
     MAIN_FILE = sys.modules['__main__'].__file__
 
-if MAIN_FILE is not None: MAIN_DIR = dirname(MAIN_FILE)
-else: MAIN_DIR = None
+if MAIN_FILE is not None: 
+    MAIN_DIR = dirname(MAIN_FILE)
+else: 
+    MAIN_DIR = None
 
 PONY_DIR = dirname(__file__)
